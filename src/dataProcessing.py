b39eb92e5cb8990a24f35d41e68b5cfb9aa697b3
Jithendra K <ee18m087@smail.iitm.ac.in>
2020-04-05 19:07:08 +0530

    Added code to generate grouped bar plot.

--- src/dataProcessing.py
+++ src/dataProcessing.py
@@ -1,5 +1,6 @@
 import numpy as np
 import csv
+import matplotlib.pyplot as pyplot
 
 #Conditional Debugging
 debug = False
@@ -13,6 +14,7 @@ adList	= [[8,8], [16,16], [32,32], [64,64], [128,128]] #list of systolic array d
 dfList	= ["os", "ws", "is"]
 nnList 	= ["AlphaGoZero", "DeepSpeech2", "FasterRCNN", "NCF_recommendation", "Resnet50", "Sentimental_seqCNN", "Transformer"]
 mdfList	= [] # List of Missing data files
+layerCount	= [8, 6, 46, 8, 54, 4, 891]
 
 ad 		= 128
 dfIndex	= 0 # Index for dataflow loop
@@ -24,33 +26,54 @@ for dfIndex, df in enumerate(dfList) :
 	for nnIndex, nn in enumerate(nnList) :
 		runFolder	= nn + "_" + str(ad) + "_" + df + "/"
 		fileName = rootFolder +  runFolder + "outputs/" + runFolder + nn + "_" + typeOfData + ".csv"
-		if (debug) :
-			print(f'File Name\t: {fileName}')
+		
+		# if (debug) :
+		# 	print(f'File Name\t: {fileName}')
+		
 		#File Parsing and data Processing
-		#fileName	= 'outputs/output_summary/AlphaGoZero_128_is/outputs/AlphaGoZero_128_is/AlphaGoZero_cycles.csv'
 		with open(fileName, mode = 'r') as file :
 			fileContent	= csv.DictReader(file)
 			lineCount	= 0
 			totalCycles	= 0
 
-			# if debug :
-			# 	print(f'fileContent dataType\t: {type(fileContent)}')
-
 			for line in fileContent:
 				if (lineCount == 0) :
-					if (debug) :
-						print('Reading CSV file')
 					lineCount += 1 #Added due to dictReader
 
 				totalCycles	+= int(line["	Cycles"])
 				lineCount	+= 1
-			if (lineCount == 0) :
+			if (lineCount != (layerCount[nnIndex] + 1)) :
 				mdfList.append(fileName)
 			if (debug) :
 				print(f'Total Cycles\t: {totalCycles}')
 				print(f'Lines read\t: {lineCount}')
 			rtCycles[dfIndex][nnIndex] = totalCycles/scaleFac
 		if debug:
-			print(f'rtCycles[{dfIndex}][{nnIndex}]\t: {rtCycles[dfIndex][nnIndex]}')
-print(f'{rtCycles}')
-print(f'{mdfList}')
+			print(f'rtCycles[{dfIndex}][{nnIndex}]\t: {rtCycles[dfIndex][nnIndex]}\n')
+if debug:
+	print(f'{rtCycles}')
+with open('outputs/missingDataFileList.txt', mode='w') as msFile :
+	msFile.writelines('%s\n' % element for element in mdfList)
+#print(f'{mdfList}')
+
+xLabel	= ['W1', 'W2', 'W3', 'W4', 'W5', 'W6', 'W7']
+x 		= np.arange(nnCount)
+fig 	= pyplot.axes()
+axes	= fig#fig.add_axes([0,0,1,1])
+barWidth= 0.25
+barColor= ['#4F81BD', '#9F4C7C', '#9BBB59']
+
+for i in range(dfCount) :
+	axes.bar(x+i*barWidth, rtCycles[i], color =barColor[i], width = barWidth, zorder=3)
+
+axes.grid(True, axis='y', zorder=0)
+axes.set_title('Cycles for array of size 128*128')
+axes.set_xlabel('Workload')
+axes.set_ylabel('Runtime in million cycles')
+axes.set_xticks([x1+barWidth for x1 in x])
+axes.set_xticklabels(xLabel)
+axes.legend(labels = dfList)
+figName = 'outputs/figures/cycles.png'
+pyplot.savefig(figName, transparent =True, format='png')
+if debug:
+	pyplot.show()
